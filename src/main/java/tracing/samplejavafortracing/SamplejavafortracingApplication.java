718d10c1ab94bac665b96372fcbe282b5416faf4
u724198 <Welcome##19>
2022-01-20 23:07:53 +0530

    first code

---
+++ src/main/java/tracing/samplejavafortracing/SamplejavafortracingApplication.java
@@ -0,0 +1,23 @@
+package tracing.samplejavafortracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+
+public class SamplejavafortracingApplication {
+
+	@GetMapping("/hello")
+	public String getMessage(){
+		return "Hello Messages!!";
+
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplejavafortracingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tracing/samplejavafortracing/SamplejavafortracingApplicationTests.java
@@ -0,0 +1,13 @@
+package tracing.samplejavafortracing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplejavafortracingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
